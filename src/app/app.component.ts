735db6c6db7dde3c584ee74f5d9bcc19b35f4cca
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-08 16:25:26 -0300

    feat: Import FontAwesomeModule in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TarefasComponent } from './tarefas/tarefas.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TarefasComponent],
+  imports: [RouterOutlet, TarefasComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
