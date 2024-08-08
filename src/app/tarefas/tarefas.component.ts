11b296a2f3493c8ed6b3bf1b9df471ad4d6904bd
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-08 16:25:50 -0300

    Refactor: TarefasComponent to import FontAwesomeModule and add task actions

--- src/app/tarefas/tarefas.component.ts
+++ src/app/tarefas/tarefas.component.ts
@@ -3,11 +3,13 @@ import { Tarefa } from '../Tarefa';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TAREFAS } from '../mock-tarefas';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faTrash, faPenSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tarefas',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor, NgIf],
+  imports: [CommonModule, FormsModule, NgFor, NgIf, FontAwesomeModule],
   templateUrl: './tarefas.component.html',
   styleUrls: ['./tarefas.component.css'],
 })
@@ -23,6 +25,10 @@ export class TarefasComponent {
     responsavel: '',
     deadline: '',
   };
+
+  faEye = faEye;
+  faTrash = faTrash;
+  faPenSquare = faPenSquare;
 
   onSelect(tarefa: Tarefa): void {
     this.selectedTarefa = tarefa;
