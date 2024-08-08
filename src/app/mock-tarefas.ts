381be30af442c69ceb7da411d33bb3dfa3143d8a
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-08 15:36:51 -0300

    feat: Add mock data for tasks

---
+++ src/app/mock-tarefas.ts
@@ -0,0 +1,40 @@
+import { Tarefa } from './Tarefa';
+
+export const TAREFAS: Tarefa[] = [
+  {
+    id: 2,
+    name: 'task 2',
+    prioridade: '',
+    projeto: '',
+    status: '',
+    responsavel: '',
+    deadline: '',
+  },
+  {
+    id: 3,
+    name: 'task 3',
+    prioridade: '',
+    projeto: '',
+    status: '',
+    responsavel: '',
+    deadline: '',
+  },
+  {
+    id: 4,
+    name: 'task 4',
+    prioridade: '',
+    projeto: '',
+    status: '',
+    responsavel: '',
+    deadline: '',
+  },
+  {
+    id: 5,
+    name: 'task 5',
+    prioridade: '',
+    projeto: '',
+    status: '',
+    responsavel: '',
+    deadline: '',
+  },
+];
