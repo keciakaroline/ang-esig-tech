85df818c6ec53c4447f2b10bb1e17f3e32c9cf20
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-09 15:10:57 -0300

    Refactor: app component structure and add sidebar and header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskComponent } from './task/task.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TaskComponent } from './task/task.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskComponent, FontAwesomeModule],
+  imports: [
+    RouterOutlet,
+    TaskComponent,
+    FontAwesomeModule,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
