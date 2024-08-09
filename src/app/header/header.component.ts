8ccb9536e3eeb3233edfd7e12ded2afbc439a3d3
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-09 15:10:38 -0300

    feat: Add header component for page header and user profile

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  faSearch = faSearch;
+}
