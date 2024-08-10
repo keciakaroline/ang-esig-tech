83cd9e84a0bca53118c203fd7c25af4d7c7516a6
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-09 22:21:28 -0300

    refactor: Update app component structure and styles, add task component and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from './task/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'tasks', component: TaskComponent }];
