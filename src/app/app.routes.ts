0f8eb6b83850923d13a078f7373c59e6d9831122
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-10 21:34:12 -0300

    feat: Add dashboard component and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { TaskComponent } from './task/task.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [{ path: 'tasks', component: TaskComponent }];
+export const routes: Routes = [
+  { path: 'tasks', component: TaskComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
