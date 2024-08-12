e83242817b095b88302aa718b206959b3ac3fb3c
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-12 14:09:20 -0300

    feat: add HttpClient and InMemoryWebApiModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()],
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      InMemoryWebApiModule.forRoot(InMemoryDataService, {
+        dataEncapsulation: false,
+        delay: 500,
+        passThruUnknownUrl: true,
+      })
+    ),
+  ],
 };
