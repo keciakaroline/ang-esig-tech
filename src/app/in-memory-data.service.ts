e9fce3e4c507e34ef47959e46facffa62dd1cf1b
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-12 14:05:49 -0300

    feat: add in-memory-data.service.spec.ts and in-memory-data.service.ts

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,60 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Injectable } from '@angular/core';
+import { Tarefa } from './Tarefa';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const tarefas: Tarefa[] = [
+      {
+        id: 1,
+        title: 'Adjust concentrates to behaviour and structure',
+        prioridade: 'Alta',
+        projeto: 'Gateway of Pacific',
+        status: 'Concluido',
+        responsavel: 'Miles, Esther',
+        deadline: '2024-08-11',
+        detalhes: 'Task 1 details',
+      },
+      {
+        id: 2,
+        title: 'Initiation/Planning Activities Completed',
+        prioridade: 'Média',
+        projeto: 'Renoir Hotel',
+        status: 'Concluido',
+        responsavel: 'Miles, Esther',
+        deadline: '2024-08-11',
+        detalhes: 'Task 2 details',
+      },
+      {
+        id: 3,
+        title: 'Requirements Analysis Completed',
+        prioridade: 'Baixa',
+        projeto: 'Grand Hyatt Union',
+        status: 'Concluido',
+        responsavel: 'Miles, Esther',
+        deadline: '2024-08-11',
+        detalhes: 'Task 3 details',
+      },
+      {
+        id: 4,
+        title: 'Security Planning',
+        prioridade: 'Alta',
+        projeto: '801 Brannan',
+        status: 'Concluido',
+        responsavel: 'Miles, Esther',
+        deadline: '2024-08-11',
+        detalhes: 'Task 3 details',
+      },
+    ];
+    return { tarefas };
+  }
+
+  genId(tarefas: Tarefa[]): number {
+    return tarefas.length > 0
+      ? Math.max(...tarefas.map((tarefa) => tarefa.id)) + 1
+      : 1;
+  }
+}
