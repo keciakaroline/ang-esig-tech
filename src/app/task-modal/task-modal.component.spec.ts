476b1608fbb53c0d8d91721abb380edbad73b547
Kecia Karoline <kecia.karoline@codeminer42.com>
2024-08-13 13:14:33 -0300

    test: add test to task-modal component

--- src/app/task-modal/task-modal.component.spec.ts
+++ src/app/task-modal/task-modal.component.spec.ts
@@ -1,18 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskModalComponent } from './task-modal.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
 
 describe('TaskModalComponent', () => {
+  let component: TaskModalComponent;
+  let fixture: ComponentFixture<TaskModalComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TaskModalComponent, HttpClientTestingModule, FormsModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(TaskModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    const fixture = TestBed.createComponent(TaskModalComponent);
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should emit modalClosed event when closeModal is called', () => {
+    spyOn(component.modalClosed, 'emit');
+
+    component.closeModal();
+
+    expect(component.modalClosed.emit).toHaveBeenCalled();
   });
 });
